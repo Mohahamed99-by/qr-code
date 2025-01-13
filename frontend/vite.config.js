4066bab0539cf0a0c20e23eb686ef72a7e2d14a0
Mohahamed99-by <mohamedtolba161@gmail.com>
2025-01-13 19:06:38 +0100

    hhh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Adjust this if hosting under a subdirectory
-  build: {
-    outDir: 'dist', // Default build output directory
-  },
-  server: {
-    port: 5173, // Optional: Set local dev server port
-  },
-});
+})
