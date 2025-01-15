2fd27736351f2ee794373ba44ef8519d3195a0cb
Mohahamed99-by <mohamedtolba161@gmail.com>
2025-01-15 15:07:05 +0100

    lastmomo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    emptyOutDir: true,
   }
 })
